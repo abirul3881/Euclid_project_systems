20212ce65a08e5a60eaa724d2e7b28cf6462d003
abirul3881 <abirul@mail.ru>
2024-05-30 19:51:46 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,1 @@
-window.addEventListener('DOMContentLoaded', function () {
-	a = document.querySelector('.main__btn')
-	a.addEventListener('click', function (event) {
-		event.target.style.backgroundColor = 'red'
-	})
-})
+document.addEventListener('DOMContentLoaded', function () {})
